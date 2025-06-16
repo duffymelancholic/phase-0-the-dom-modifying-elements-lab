af05c74f838cf0fa89a661544e5906431190e19f
Nicholas obonyo abala <nicholasobonyo444@gmail.com>
2025-06-16 14:33:36 +0300

    All passing tests

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+function removeMain(mainTagId) {
+    const mainTag = document.getElementById(mainTagId);
+    mainTag.remove();
+}
+removeMain("main");
+
+const newHeader = document.createElement('h1'); // Create an <h1> element
+newHeader.id = 'victory'; // Set the id to 'victory'
+newHeader.textContent = 'Abala is the champion'; // Set the text content
